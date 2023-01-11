3811feb8930d1a703500ba1bb84c4d0b586003ef
Daniel Ritter <head@daniel-ritter.biz>
2023-01-11 18:27:34 +0100

    Add crafting monitor for Powah Energizing Orb

---
+++ powah/energizing_orb.lua
@@ -0,0 +1,120 @@
+require "peripherals"
+
+  -- Energizing Orb
+local crafter = Inventory:new("powah:energizing_orb_0", SlotRange:new(2), SlotRange:new(1, 1))
+
+--   -- Chest which receives all items for crafting recipe
+-- local inChest = Inventory:new("ironchest:iron_chest_3")
+
+  -- Chest which receives crafted items
+local outChest = Inventory:new("ironchest:iron_chest_5")
+
+  -- RS crafter, to receive redstone pulse for next item set (redrouter instance)
+local rsCrafter = RedstoneSignalSender("redstoneIntegrator_0", "back")
+
+
+local config = {
+  tick = 1, -- seconds
+  debug = false
+  -- crafterOutItemSlot = 1
+}
+
+local state = {
+  startup = "STARTUP",
+  idle = "IDLE",
+  crafting = "CRAFTING"
+}
+
+local params = {...}
+config["debug"] = #params >= 1 and params[1] == "-d"
+
+
+local function debug(...)
+  if config.debug then
+    print(...)
+  end
+end
+
+local function craftingHandler()
+  local subscribedEvents = {
+    inItemAvailable = true,
+    inItemNotAvailable = true,
+    craftedItemAvailable = true,
+    craftedItemNotAvailable = true
+  }
+  local currentState = state.startup
+
+  repeat
+    local event, param = coroutine.yield()
+    if subscribedEvents[event] then
+      debug("Crafter - Event - " .. event)
+      if event == "inItemAvailable" and currentState ~= state.startup then
+        if currentState == state.idle then
+          currentState = state.crafting
+        end
+        local itemStack = param
+        debug("Crafter - IA:", itemStack.name, " STATE:", currentState)
+        -- crafter.importItemStack(itemStack)
+        os.queueEvent("inChestCheckNextItem")
+      elseif event == "outItemAvailable" then
+        local itemStack = param
+        local result = outChest.importItemStack(itemStack)
+        if result == 0 then
+          currentState = state.idle
+        end
+      elseif event == "outItemNotAvailable" and currentState == state.startup then
+        currentState = state.idle
+      elseif event == "inItemNotAvailable" and currentState == state.idle then
+        rsCrafter.toggleRedstoneOutput()
+      end
+    end
+  until event == "terminate"
+end
+
+local function inChestMonitor()
+  local subscribedEvents = {
+    timer = true,
+    inChestCheckNextItem = true
+  }
+  repeat
+    local event, _ = coroutine.yield()
+    if subscribedEvents[event] then
+      debug("inChestMonitor - Event - ".. event)
+      local itemStack = crafter.nextItemStack()
+      if itemStack then
+        debug("inChestMonitor - Item:".. itemStack.displayName)
+        os.queueEvent("inItemAvailable", itemStack)
+      else
+        os.queueEvent("inItemNotAvailable")
+      end
+    end
+  until event == "terminate"
+end
+
+local function craftedItemMonitor()
+  repeat
+    debug("craftedItemMonitor - Timer Event")
+    local itemStack = crafter:nextItemStack()
+    if itemStack then
+      debug("inChestMonitor - Item:".. itemStack.displayName)
+      os.queueEvent("craftedItemAvailable", itemStack)
+    else
+      os.queueEvent("craftedItemNotAvailable")
+    end
+    local event = coroutine.yield("timer")
+  until event[1] == "terminate"
+end
+
+local function pulseGenerator()
+  repeat
+      os.startTimer(config.tick)
+      local event = coroutine.yield("timer")
+  until event == "terminate"
+end
+
+parallel.waitForAny(
+  inChestMonitor,
+  craftedItemMonitor,
+  pulseGenerator,
+  craftingHandler
+)
---
+++ powah/peripherals.lua
@@ -0,0 +1,93 @@
+SlotRange = {
+  first = nil,
+  last = nil
+}
+
+function SlotRange:new(params, maxSlots)
+  local o = {
+    first = params.first,
+    last = params.last
+  }
+  if o.first == nil then
+    o.first = 1
+  end
+  if o.last == nil then
+    o.last = maxSlots
+  end
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+Inventory = {
+  device = nil,
+  name = "",
+  inSlots = nil,
+  outSlots = nil
+}
+
+function Inventory:new(deviceName, inSlotParams, outSlotParams)
+  local o = {
+    device = peripheral.wrap(deviceName),
+    name = deviceName
+  }
+  o.outSlots = SlotRange:new(outSlotParams, o.device.size())
+  o.inSlots = SlotRange:new(inSlotParams. o.device.size())
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+function Inventory:nextItemStack()
+  for slot = self.outSlots.first, self.outSlots.last do
+    local itemStack = self.device.getItemDetail(slot)
+    if itemStack then
+      itemStack.slot = slot
+      itemStack.inventory = self
+      return itemStack
+    end
+  end
+  return nil
+end
+
+function Inventory:importItemStack(itemStack)
+  local remainder = itemStack.count
+  for slot = self.inSlots.first, self.inSlots.last do
+    local slotItem = self.device.getItemDetail(slot)
+    if slotItem == nil or slotItem.name == itemStack.name then
+      local movedItemCount = self.device.pullItems(itemStack.inventory.name, remainder, itemStack.slot)
+      if movedItemCount == remainder then
+        break
+      else
+        remainder = remainder - movedItemCount
+      end
+    end
+  end
+  return remainder
+end
+
+RedstoneSignalSender = {
+  device = nil,
+  name = "",
+  side = nil
+}
+
+function RedstoneSignalSender:new(deviceName, side)
+  local o = {
+    device = peripheral.wrap(deviceName),
+    name = deviceName,
+    side = side
+  }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+function RedstoneSignalSender:setOutput(on)
+  self.device.setOutput(self.side, on)
+end
+
+function RedstoneSignalSender:toggleOutput()
+  local state = self.device.getOutput(self.side)
+  self.device.setOutput(self.side, not state)
+end
